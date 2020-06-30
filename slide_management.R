e50645be011164d696dfa78eb021397e7c5c0aca
cerees <chris.e.rees@gmail.com>
2020-06-30 16:32:26 -0400

    publishing deck

---
+++ slide_management.R
@@ -0,0 +1,22 @@
+#pkgs <- c('ramnathv/slidifyLibraries', 'ramnathv/slidify')
+#devtools::install_github(pkgs)
+
+##################################
+####LOAD FUNCTIONS AND PACKAGES###
+##################################
+library(slidify)
+library(slidifyLibraries)
+library(diagram)
+library(extrafont)
+library(RSvgDevice)
+################################
+###CREATE DIAGRAMS AND SLIDES###
+################################
+
+###AUTHOR SLIDES###
+author("~/data_science/cdphp_slide_template")
+###CREATE HTML FILE###
+slidify("~/data_science/cdphp_slide_template/index.Rmd")
+###PUBLSIH SLIDES###
+publish(user = "cerees", repo = "nlp_customer_intimacy")
+
